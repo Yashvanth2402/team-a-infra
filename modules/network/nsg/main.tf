24103785d636112cee7443c56d0908a70b925c81
Yashvanth2402 <yashvanthbalaji2402@gmail.com>
2026-02-02 16:29:39 +0530

    Create main.tf

---
+++ modules/network/nsg/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_network_security_group" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_network_security_rule" "allow_https_outbound" {
+  name                        = "allow-https-outbound"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.this.name
+}
