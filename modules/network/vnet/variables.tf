86d379547d55672b10457a8cbc48deacdbc2571d
Yashvanth2402 <yashvanthbalaji2402@gmail.com>
2026-02-02 15:55:47 +0530

    Create variables.tf

---
+++ modules/network/vnet/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  description = "VNet name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "address_space" {
+  description = "VNet address space"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags"
+  type        = map(string)
+}
