1683a92fed2de67051269207e5c0e0d18f40b818
Yashvanth2402 <yashvanthbalaji2402@gmail.com>
2026-02-02 21:28:20 +0530

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "rg-tfstate"
-    storage_account_name  = "tfstateplatform"
-    container_name        = "dev"
-    key                   = "infra.tfstate"
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstateplatform01"
+    container_name       = "dev"
+    key                  = "infra.tfstate"
 
     use_azuread_auth = true
   }
