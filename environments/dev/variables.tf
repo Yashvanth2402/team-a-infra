e14b18b3e11afb98759e11fcf99d84b316ea6f4c
Yashvanth2402 <yashvanthbalaji2402@gmail.com>
2026-02-02 15:46:01 +0530

    Create variables.tf

---
+++ environments/dev/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+}
